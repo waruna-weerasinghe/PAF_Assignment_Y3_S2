a81bef37ee8a807755ca1f3ab987cbaa34634b0b
DRK-SIDE <161360258+DRK-SIDE@users.noreply.github.com>
2025-04-29 17:34:22 +0530

    update

---
+++ backend/demo/src/main/java/com/PAF_Assignment/demo/controller/PostController.java
@@ -0,0 +1,36 @@
+package com.PAF_Assignment.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.PAF_Assignment.demo.Model.Post;
+import com.PAF_Assignment.demo.Repository.PostRepo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping
+@CrossOrigin
+
+public class PostController {
+
+@Autowired
+PostRepo postRepo;
+
+    @PostMapping("/addPost")
+    public void addPost(@RequestBody Post post) {
+        postRepo.save(post);
+        
+    }
+        
+    
+    
+    
+
+}
