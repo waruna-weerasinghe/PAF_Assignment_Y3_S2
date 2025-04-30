5c6fae85edd96eed10f77a3e4f27f3164cd228a9
Rushan Avishka <rushanavishka4@gmail.com>
2025-04-30 14:11:51 +0530

    Create cakeRecipeRepo.java

---
+++ backend/demo/src/main/java/com/PAF_Assignment/demo/Repository/cakeRecipeRepo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.PAF_Assignment.demo.Repository;
+
+import com.PAF_Assignment.demo.Model.cakeRecipe;
+
+public class cakeRecipeRepo {
+
+    public void save(cakeRecipe user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+}
