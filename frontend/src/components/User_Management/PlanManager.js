03f578a07ba7a3b1345386288e04c72934a726ed
Waruna Weerasinghe <134158497+waruna-weerasinghe@users.noreply.github.com>
2025-05-13 14:35:59 +0530

    Delete frontend directory

--- frontend/src/components/User_Management/PlanManager.js
+++
@@ -1,5 +0,0 @@
-function PlanManager() {
-    return <div><h2>Plan Management</h2><p>Manage subscription or other plans here.</p></div>;
-  }
-  export default PlanManager;
-  
--- frontend/src/components/User_Management/PostManager.js
+++
@@ -1,5 +0,0 @@
-function PostManager() {
-    return <div><h2>Post Management</h2><p>Manage posts here.</p></div>;
-  }
-  export default PostManager;
-  
