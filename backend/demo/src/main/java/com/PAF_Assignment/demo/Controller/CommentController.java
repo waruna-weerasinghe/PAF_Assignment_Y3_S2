c4225f5acde3948ccd8bb9d3362e9c71ac16f237
DilukaB <navanjana721@gmail.com>
2025-05-14 00:49:58 +0530

    updating

---
+++ backend/demo/src/main/java/com/PAF_Assignment/demo/Controller/CommentController.java
@@ -0,0 +1,26 @@
+package com.PAF_Assignment.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.PAF_Assignment.demo.Entity.Comment;
+import com.PAF_Assignment.demo.Repository.CommentRepo;
+import com.PAF_Assignment.demo.Service.CommentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping("/addComment")
+    public Comment postDetails(@RequestBody Comment comment) {
+
+        return commentService.saveDetails(comment);
+
+    }
+
+}
