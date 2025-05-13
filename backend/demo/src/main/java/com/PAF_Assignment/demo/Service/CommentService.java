87ba235fbf69afb35c02ecac54e412c6fbcd3130
DilukaB <navanjana721@gmail.com>
2025-05-14 02:20:33 +0530

    Update CommentService.java

--- backend/demo/src/main/java/com/PAF_Assignment/demo/Service/CommentService.java
+++ backend/demo/src/main/java/com/PAF_Assignment/demo/Service/CommentService.java
@@ -36,7 +36,7 @@ public class CommentService {
     public String deleteComment(int id) {
 
         commentRepo.deleteById(id);
-        return "deleted" + id;
+        return "Comment is deleted" + id;
 
     }
 
