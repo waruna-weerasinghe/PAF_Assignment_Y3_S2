187b6dbf7aa17255b9eb6442c41c4c80dad48858
DilukaB <navanjana721@gmail.com>
2025-05-13 22:54:33 +0530

    y

--- backend/demo/src/main/java/com/PAF_Assignment/demo/Repository/Plan.java
+++
@@ -1,9 +0,0 @@
-@Document(collection = "plan")
-public class Plan {
-    @Id
-    private String id;
-    private String rname;
-    private String ingredients;
-    private String category;
-    // Getters & Setters if not using Lombok
-}
--- backend/demo/src/main/java/com/PAF_Assignment/demo/Repository/PostRepo.java
+++
@@ -1,9 +0,0 @@
-package com.PAF_Assignment.demo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.PAF_Assignment.demo.Model.Post;
-
-public interface PostRepo extends MongoRepository<Post, String> {
-
-}
