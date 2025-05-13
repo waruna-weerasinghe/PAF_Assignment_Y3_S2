d8865de49045c6cf332977225ca3eea5aeaba048
DilukaB <navanjana721@gmail.com>
2025-05-13 11:23:47 +0530

    Update Singup.jsx

--- New_Frontend/src/Components/Register/Singup.jsx
+++ New_Frontend/src/Components/Register/Singup.jsx
@@ -40,7 +40,7 @@ const Signup = () => {
             alt="Cake Shop Logo"
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sweet Delights
+            Yes! We Cake
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Experience the delightful world of baking with Us
